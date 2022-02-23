ad80dd6d227452b6699f4e8c5ce15f512e14f13c
Kadi Koppelmann <kadikoppelmann@gmail.com>
2022-02-23 13:54:00 +0200

    updated stuff

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 import VueAxios from "vue-axios";
-import datePicker from "vue-bootstrap-datetimepicker";
 
-// Vue.use(datePicker);
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
-// new Vue({
-//     render: h => h(App)
-// }).$mount('#app')
+
 
 
 new Vue({
