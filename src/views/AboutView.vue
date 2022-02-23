fc0bb4b3a4dd0c3928d07ba9a1f80c3336df5acb
Kadi Koppelmann <kadikoppelmann@gmail.com>
2022-02-23 14:00:08 +0200

    updated stuff

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,6 +7,9 @@
     <h3>Siia tuleb tekst meie asutuse kohta jne jne jne jne</h3>
     <br>
     <br>
+    <h3>Soovime meie koertele parimat elu</h3>
+    <br>
+    <br>
     <br>
     <h3>Siia tuleb pilt jne jne jne jne</h3>
 
